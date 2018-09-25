573ee2a6fe171cede9fb5bdc4940c1bb2980142c
eastskim <eastskim48@gmail.com>
2018-09-25 15:33:15 +0900

    tmap 지도 연동

--- app/src/main/java/dongkyul/pospot/view/main/MainMapActivity.java
+++ app/src/main/java/dongkyul/pospot/view/main/MainMapActivity.java
@@ -1,11 +1,7 @@
 package dongkyul.pospot.view.main;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.support.constraint.ConstraintLayout;
 
 import com.skt.Tmap.TMapView;
 
@@ -18,20 +14,23 @@ public class MainMapActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_map);
+        init();
+    }
 
-        LinearLayout viewMap = (LinearLayout)findViewById(R.id.viewMap);
-        TMapView tMapView = new TMapView(this);
-        tMapView.setSKTMapApiKey( "27c0a585-2760-4f43-afb6-cba605ab31b3" );
+    @Override
+    public void init() {
+        super.init();
+        ConstraintLayout layout = (ConstraintLayout)findViewById(R.id.layout);
+        TMapView tMapView = (TMapView)findViewById(R.id.tmapView);
+        tMapView.setSKTMapApiKey( "fac21bdf-e297-4eaa-b2a0-fc02db2f6f1f");
         tMapView.setLocationPoint(126.970325,37.556152);
         tMapView.setCenterPoint(126.970325,37.556152);
         tMapView.setCompassMode(true);
         tMapView.setIconVisibility(true);
         tMapView.setZoomLevel(15);
-        tMapView.setLanguage(TMapView.MAPTYPE_STANDARD);
-        tMapView.setMapType(TMapView.LANGUAGE_KOREAN);
+        tMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+        tMapView.setMapType(TMapView.MAPTYPE_STANDARD);
         tMapView.setTrackingMode(true);
         tMapView.setSightVisible(true);
-        viewMap.addView( tMapView );
-
     }
 }
