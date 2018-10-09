62db60bd0f4d1db8866ea48fca728de1acc001ef
jiniC <jinic323@gmail.com>
2018-10-09 13:25:59 +0900

    현재위치 표시

--- app/src/main/java/dongkyul/pospot/view/main/TourApiActivity.java
+++ app/src/main/java/dongkyul/pospot/view/main/TourApiActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -14,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -48,6 +50,18 @@ public class TourApiActivity extends BaseActivity {
     String tourItemTitle;
     String result = "\n";
 
+    private Button btnShowLocation;
+    private TextView txtLat;
+    private TextView txtLon;
+    private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
+    private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
+    private boolean isAccessFineLocation = false;
+    private boolean isAccessCoarseLocation = false;
+    private boolean isPermission = false;
+
+    // GPSTracker class
+    private GpsInfo gps;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,9 +70,44 @@ public class TourApiActivity extends BaseActivity {
         mapX = 126.981611;
         mapY = 37.568477;
         getTourData(mapX,mapY);
+
+        // GPS 정보를 보여주기 위한 이벤트 클래스 등록
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // 권한 요청을 해야 함
+                if (!isPermission) {
+                    callPermission();
+                    return;
+                }
+
+                gps = new GpsInfo(TourApiActivity.this);
+                // GPS 사용유무 가져오기
+                if (gps.isGetLocation()) {
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    txtLat.setText(String.valueOf(latitude));
+                    txtLon.setText(String.valueOf(longitude));
+
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "당신의 위치 - \n위도: " + latitude + "\n경도: " + longitude,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    // GPS 를 사용할수 없으므로
+                    gps.showSettingsAlert();
+                }
+            }
+        });
+
+        callPermission();  // 권한 요청을 해야 함
     }
 
     private void initView(){
+        btnShowLocation = (Button) findViewById(R.id.btn_start);
+        txtLat = (TextView) findViewById(R.id.tv_latitude);
+        txtLon = (TextView) findViewById(R.id.tv_longitude);
         tourapi = (TextView) findViewById(R.id.tourapi);
     }
 
@@ -91,5 +140,47 @@ public class TourApiActivity extends BaseActivity {
             public void onFailure(@NonNull Call<JsonObject> call, @NonNull Throwable t) {
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_ACCESS_FINE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            isAccessFineLocation = true;
+
+        } else if (requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            isAccessCoarseLocation = true;
+        }
+
+        if (isAccessFineLocation && isAccessCoarseLocation) {
+            isPermission = true;
+        }
+    }
+
+    // 전화번호 권한 요청
+    private void callPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_ACCESS_FINE_LOCATION);
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_ACCESS_COARSE_LOCATION);
+        } else {
+            isPermission = true;
+        }
     }
 }
