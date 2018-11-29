36992ead3e525718a4b2b4ed1e7a71ffa2b2d030
jiniC <jinic323@gmail.com>
2018-11-29 12:39:14 +0900

    레이아웃 수정중

--- app/src/main/java/dongkyul/pospot/view/main/PhotoMyAdapter.java
+++ app/src/main/java/dongkyul/pospot/view/main/PhotoMyAdapter.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -109,7 +110,7 @@ public class PhotoMyAdapter extends RecyclerView.Adapter<PhotoMyAdapter.PhotoVie
     class PhotoViewHolder extends RecyclerView.ViewHolder {
 
         ImageView mPhoto;
-        Button addBtn;
+        ImageButton addBtn;
 
         public PhotoViewHolder(View itemView) {
             super(itemView);
