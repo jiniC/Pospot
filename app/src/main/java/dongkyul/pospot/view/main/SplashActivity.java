052412db33b74602a5e6ad041ca7e1742a05b9aa
jiniC <jinic323@gmail.com>
2018-11-30 22:24:30 +0900

    스플래시 화면 생성

---
+++ app/src/main/java/dongkyul/pospot/view/main/SplashActivity.java
@@ -0,0 +1,18 @@
+package dongkyul.pospot.view.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(3000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, MainMapActivity.class));
+        finish();
+    }
+}
