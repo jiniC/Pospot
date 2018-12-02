bc3c770ee14f2d631df775a9c0a83a86847581b1
jiniC <jinic323@gmail.com>
2018-12-02 16:31:16 +0900

    포토마커 삭제기능 추가

--- app/src/main/java/dongkyul/pospot/view/main/PhotoMarkerViewActivity.java
+++ app/src/main/java/dongkyul/pospot/view/main/PhotoMarkerViewActivity.java
@@ -1,5 +1,7 @@
 package dongkyul.pospot.view.main;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,7 +9,11 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.skt.Tmap.TMapPoint;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +22,7 @@ import dongkyul.pospot.R;
 import dongkyul.pospot.view.common.BaseActivity;
 import io.realm.Realm;
 import io.realm.RealmList;
+import io.realm.RealmResults;
 
 public class PhotoMarkerViewActivity extends BaseActivity {
 
@@ -26,6 +33,8 @@ public class PhotoMarkerViewActivity extends BaseActivity {
     List<Bitmap> mPhotoList_img;
     RealmList<byte[]> mPhotoList_byte;
     TextView nameTab;
+    Button btnDelete;
+    Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +47,14 @@ public class PhotoMarkerViewActivity extends BaseActivity {
     public void init() {
         super.init();
         nameTab = (TextView)findViewById(R.id.markerNameTab);
+        btnDelete = (Button)findViewById(R.id.btnDelete);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
-        Double photoMarkerLat = extras.getDouble("lat");
-        Double photoMarkerLon = extras.getDouble("lon");
+        photoMarkerLat = extras.getDouble("lat");
+        photoMarkerLon = extras.getDouble("lon");
 
-        Realm realm = Realm.getDefaultInstance();
+        realm = Realm.getDefaultInstance();
         PhotoMarkerDB clickPhotoMarker;
         clickPhotoMarker = realm.where(PhotoMarkerDB.class)
                 .equalTo("lat", photoMarkerLat)
@@ -69,5 +79,41 @@ public class PhotoMarkerViewActivity extends BaseActivity {
         mRecyclerView.setLayoutManager(mGridLayoutManager);
         myAdapter = new PhotoMarkerViewAdapter(PhotoMarkerViewActivity.this, mPhotoList_img);
         mRecyclerView.setAdapter(myAdapter);
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(PhotoMarkerViewActivity.this);
+
+                alertDialog.setTitle("포토 마커 삭제");
+                alertDialog.setMessage("해당 포토마커를 삭제할까요?");
+
+                alertDialog.setPositiveButton("네",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int which) {
+                                RealmResults<PhotoMarkerDB> item = realm.where(PhotoMarkerDB.class)
+                                        .equalTo("lat", photoMarkerLat)
+                                        .and()
+                                        .equalTo("lon", photoMarkerLon)
+                                        .findAll();
+                                realm.beginTransaction();
+                                item.deleteAllFromRealm();
+                                realm.commitTransaction();
+                                realm.close();
+                                Intent intent = new Intent(PhotoMarkerViewActivity.this, MainMapActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                alertDialog.setNegativeButton("아니요",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                alertDialog.show();
+            }
+        });
     }
 }
